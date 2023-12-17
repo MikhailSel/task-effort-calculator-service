2b35b58394641e1ced28e5a0b18908f057e92e21
Igor Simakov <i.simakov@banki.ru>
2023-12-18 00:09:16 +0800

    added example controller + swagger

--- src/main/java/ru/seliverstov/userservice/User.java
+++ src/main/java/ru/seliverstov/userservice/User.java
@@ -6,26 +6,27 @@ public class User {
     private Long id;
     private String email;
 
-    public Long getId() {
-        return id;
+    public User(String email) {
+        this.id = new Random().nextLong(1000);
+        this.email = email;
     }
 
-    public String getEmail() {
-        return email;
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public User(String email) {
-        this.id = new Random().nextLong(1000);
-        this.email = email;
-    }
-
-
 }
--- src/main/java/ru/seliverstov/userservice/UserController.java
+++ src/main/java/ru/seliverstov/userservice/UserController.java
@@ -1,17 +1,44 @@
 package ru.seliverstov.userservice;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
-    @GetMapping ("api/v1/users")
-    public List <User> getUsers() {
-        return List.of(
-                new User("mail@mail.ru"),
-                new User("mail1@mail1.ru")
-        );
+    private final List<User> users = new ArrayList<>();
+
+    @GetMapping("api/v1/users")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @PostMapping("api/v1/users")
+    public User postUser(@RequestBody User user) {
+        users.add(user);
+        return user;
+    }
+
+    @DeleteMapping("api/v1/users")
+    public void deleteUser(@RequestParam Long id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
+
+    @PutMapping("api/v1/users")
+    public User updateUser(@RequestBody User request) {
+        User foundUser = users.stream()
+            .filter(u -> u.getId().equals(request.getId()))
+            .findFirst()
+            .orElseThrow();
+
+        foundUser.setEmail(request.getEmail());
+        return foundUser;
     }
 }
