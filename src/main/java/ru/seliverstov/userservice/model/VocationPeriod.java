2c69bda96cb8fce58c82032e4bb08fa8ebb8d47e
Михаил <seliverstov8@yandex.ru>
2024-03-24 20:14:12 +0500

    Добавил Lambok в VocationPeriod

--- src/main/java/ru/seliverstov/userservice/model/VocationPeriod.java
+++ src/main/java/ru/seliverstov/userservice/model/VocationPeriod.java
@@ -1,7 +1,16 @@
 package ru.seliverstov.userservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
-import java.util.Random;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class VocationPeriod {
 
@@ -13,42 +22,4 @@ public class VocationPeriod {
 
     private LocalDate dateTo;
 
-    public VocationPeriod(final Long userId, final LocalDate dateFrom, final LocalDate dateTo) {
-        this.id = new Random().nextLong(1000);
-        this.userId = userId;
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(final Long userId) {
-        this.userId = userId;
-    }
-
-    public LocalDate getDateFrom() {
-        return dateFrom;
-    }
-
-    public void setDateFrom(final LocalDate dateFrom) {
-        this.dateFrom = dateFrom;
-    }
-
-    public LocalDate getDateTo() {
-        return dateTo;
-    }
-
-    public void setDateTo(final LocalDate dateTo) {
-        this.dateTo = dateTo;
-    }
 }
