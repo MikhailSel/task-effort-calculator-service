fbe579c58e11f02362135b787445704e033932b8
Igor Simakov <i.simakov@banki.ru>
2024-04-30 14:15:02 +0300

    feat: Разделили монолитное приложение. Вынес сервис task-effort-calculator-user-service

---
+++ task-effort-calculator-user-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   BIGSERIAL,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    CONSTRAINT roles_pkey_id PRIMARY KEY (id)
+);
+
+comment on column roles.id is 'Role ID';
+comment on column roles.name is 'Role Name';
+
+INSERT INTO roles (name)
+VALUES ('Analytic');
+INSERT INTO roles (name)
+VALUES ('BackDev');
+INSERT INTO roles (name)
+VALUES ('FrontDev');
+INSERT INTO roles (name)
+VALUES ('Tester');
+
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id      BIGSERIAL,
+    fio     VARCHAR(100) NOT NULL UNIQUE,
+    id_role BIGINT       NOT NULL,
+    CONSTRAINT fk_constraint_role FOREIGN KEY (id_role) REFERENCES roles (id),
+    CONSTRAINT users_pkey_id PRIMARY KEY (id)
+);
+
+comment on column users.id is 'user ID';
+comment on column users.fio is 'user Fio';
+
+
+CREATE TABLE IF NOT EXISTS vocation_period
+(
+    id        BIGSERIAL,
+    user_id   BIGINT NOT NULL,
+    date_from DATE   NOT NULL,
+    date_to   DATE   NOT NULL,
+    CONSTRAINT vocation_period_pkey_id PRIMARY KEY (id),
+    CONSTRAINT fk_vocation_period_on_users FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+comment on table vocation_period is 'Таблица периодов отпусков';
+comment on column vocation_period.id is 'Идентификатор периода отпуска';
+comment on column vocation_period.user_id is 'Идентификатор пользователя';
+comment on column vocation_period.date_from is 'Дата начала отпуска';
+comment on column vocation_period.date_to is 'Дата окончания отпуска';
+
+
